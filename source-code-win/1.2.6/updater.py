b9a99844dd6298fa27dea9aeb766fd8597ae89d8
seevik2580 <seevik@seznam.cz>
2020-08-28 22:22:55 +0200

    fix updater

--- source-code-win/1.2.6/updater.py
+++ source-code-win/1.2.6/updater.py
@@ -1,34 +1,5 @@
 #! /usr/bin/env python
-import os
-import urllib.request
-import subprocess, shutil, time
-
-def download(url=None, write=None):
-    
-    file_name = url.split('/')[-1]
-    req = urllib.request.Request(
-        url, 
-        data=None, 
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-        }
-    )
-
-    u = urllib.request.urlopen(req)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    totalFileSize = int(meta['Content-Length'])
-    bufferSize = 9192
-    count    = fielSize = 0
-    
-    while True:
-        buffer = u.read(bufferSize)
-        if not buffer:
-            break
-        fielSize += len(buffer)
-        f.write(buffer)
-
-    f.close()
+import subprocess, shutil, time, os
 
 try:
     if os.path.exists('ipchanger.rar'):
