0a15d23f77d26f4ab824ba1cf473321bece6711d
seevik2580 <seevik@seznam.cz>
2020-08-30 21:19:17 +0200

    1.2.7-1 windows

---
+++ source-code-win/1.2.7/ipchanger.py
@@ -0,0 +1,18 @@
+import start, subprocess, os, time
+
+#vykresleni aplikace
+if __name__ == '__main__':
+    try:
+        mw = start.IpChanger()
+        mw.mainloop()
+    finally:
+        #os._exit(1)
+        #zabij vse co jsi pustil
+        SW_HIDE = 0
+        info = subprocess.STARTUPINFO()
+        info.dwFlags = subprocess.STARTF_USESHOWWINDOW
+        info.wShowWindow = SW_HIDE
+        subprocess.Popen(r'taskkill /f /im tor.exe', startupinfo=info)
+        subprocess.Popen(r'taskkill /f /im obfs4proxy.exe', startupinfo=info)
+        subprocess.Popen(r'taskkill /f /im tail.exe', startupinfo=info)
+        os._exit(1)
